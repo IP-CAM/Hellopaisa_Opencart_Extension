b9cc85c0027148c04edfc3084dbddb571d88dc78
Yujesh kc <ujesh.kc@gmail.com>
2014-01-07 15:13:10 +0545

    Still on development
    This Hello Paisa payment is still not complete.
    It is in development phase.

---
+++ admin/language/english/payment/hellopaisa.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'Hello Paisa';
+
+// User Id
+$_['hellopaisa_id']		= 'Hello Paisa ID';
+ 
+// Text 
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified hello paisa details!';
+
+// Entry
+$_['entry_hello']         = 'Hello Paisa Instructions:';
+$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort Order:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify hello paisa transfer!';
+$_['error_hello']         = 'Hello Paisa Instructions Required!';
+?>
---
+++ catalog/language/english/payment/hellopaisa.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']       = 'Hello Paisa';
+$_['text_instruction'] = 'Hello Paisa Instructions';
+$_['text_description'] = 'Please transfer the total amount to the following hello paisa account.';
+$_['text_payment']     = 'Your order will not ship until we receive payment.';
+?>
